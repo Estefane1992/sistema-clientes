44e5170de004c41bf3d4043408c7ea6ce34983f0
Estefane1992 <fannyczs2012@gmail.com>
2023-12-29 18:21:03 -0300

    Atualização

--- src/main/java/io/github/estefane1992/clientes/rest/ClienteController.java
+++ src/main/java/io/github/estefane1992/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.estefane1992.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -21,5 +22,29 @@ public class ClienteController {
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
     }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete( @PathVariable Integer id) {
+        repository.findById(id).map(cliente -> {repository.delete(cliente);
+            return Void.TYPE;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar ( @PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+        repository.findById(id).map(cliente -> {
+            clienteAtualizado.setId(cliente.getId());
+            return repository.save(clienteAtualizado);
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 
 }
