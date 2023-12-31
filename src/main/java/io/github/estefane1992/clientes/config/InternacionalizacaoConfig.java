6ccab5216d18e6098c810f7757ad5b2c88a295a8
Estefane1992 <fannyczs2012@gmail.com>
2023-12-31 19:31:36 -0300

    Atualização

--- src/main/java/io/github/estefane1992/clientes/config/InternacionalizacaoConfig.java
+++ src/main/java/io/github/estefane1992/clientes/config/InternacionalizacaoConfig.java
@@ -10,20 +10,20 @@ import java.util.Locale;
 
 @Configuration
 public class InternacionalizacaoConfig {
+
     @Bean
     public MessageSource messageSource(){
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+        messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("ISO-8859-1");
-        messageSource.setDefaultLocale(Locale.getDefault());
+        messageSource.setDefaultLocale( Locale.getDefault() );
         return messageSource;
-
     }
+
     @Bean
     public LocalValidatorFactoryBean validatorFactoryBean(){
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
     }
-
 }
--- src/main/java/io/github/estefane1992/clientes/rest/ClienteController.java
+++ src/main/java/io/github/estefane1992/clientes/rest/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.estefane1992.clientes.rest;
 
 import io.github.estefane1992.clientes.model.entily.Cliente;
 import io.github.estefane1992.clientes.model.repository.ClienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,31 +20,35 @@ public class ClienteController {
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente) {
+    public Cliente salvar( @RequestBody @Valid Cliente cliente ){
         return repository.save(cliente);
     }
 
     @GetMapping("{id}")
-    public Cliente acharPorId(@PathVariable Integer id){
-        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
+    public Cliente acharPorId( @PathVariable Integer id ){
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete( @PathVariable Integer id) {
-        repository.findById(id).map(cliente -> {repository.delete(cliente);
-            return Void.TYPE;
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public void deletar( @PathVariable Integer id ){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
-
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar ( @PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+    public void atualizar ( @PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado) {
         repository.findById(id).map(cliente -> {
             clienteAtualizado.setId(cliente.getId());
             return repository.save(clienteAtualizado);
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
 
